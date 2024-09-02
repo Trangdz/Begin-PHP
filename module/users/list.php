c10dd8fd3fa029ea050240acadf6c594e964123b
Mr.Trang <trinhvantrangok12321@gmail.com>
2024-09-02 23:46:29 +0700

    Add function edit

--- module/users/list.php
+++ module/users/list.php
@@ -13,11 +13,11 @@ if (isLogin()) {
 
     if (isGet()) {
         $body = getBody();
-    
+
         // Xử lý lọc status
         if (!empty($body['status'])) {
             $status = $body['status'];
-    
+
             // Điều kiện để xác định giá trị statusSql
             $statusSql = ($status == 2) ? 0 : $status;
             if (str_contains($filter, 'WHERE')) {
@@ -27,9 +27,9 @@ if (isLogin()) {
                 $filter .= "WHERE status = $statusSql";
             }
         }
-    
-      
-    
+
+
+
         if (!empty($body['keyword'])) {
             $keyword = $body['keyword'];
             if (str_contains($filter, 'WHERE') === false) {
@@ -75,22 +75,26 @@ if (isLogin()) {
     if (!empty($_SERVER['QUERY_STRING'])) {
         $queryString = $_SERVER['QUERY_STRING'];
         $queryString = str_replace('module=users', '', $queryString);
-        $queryString = str_replace('page='.$page, '', $queryString);
+        $queryString = str_replace('page=' . $page, '', $queryString);
         $queryString = trim($queryString, '&');
         $queryString = '&' . $queryString;
     }
-    
+
     echo $queryString;
-    
-    
+    $msg = getFlashData('msg');
+    $msg_type = getFlashData('msg_type');
+
 ?>
     <div class="container">
         <hr />
         <h3>Quản lý người dùng</h3>
+        <?php
+              getMsg($msg, $msg_type);
+        ?>
         <a href="?module=users&action=add" class="btn btn-success btn-sm"><i class="fa fa-plus"></i>
             Add user</a>
-
-        <div class="tool-search">
+        <
+            <div class="tool-search">
             <form action="" method="get">
                 <div class="row">
                     <div class="col-3">
@@ -114,90 +118,90 @@ if (isLogin()) {
 
                 <input type="hidden" name="module" value="users">
             </form>
-        </div>
+    </div>
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th width="5%">Serial</th>
-                    <th>Fullname</th>
-                    <th>Email</th>
-                    <th>Phone number</th>
-                    <th>Status</th>
-                    <th width="5%">Sửa</th>
-                    <th width="5%">Xóa</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (!empty($listAllUser)) : ?>
-                    <?php
-                    $count = 0; // Khởi tạo biến đếm để hiển thị số thứ tự
-                    foreach ($listAllUser as $item) :
-                        $count++;
-                    ?>
-                        <tr>
-                            <td><?php echo $count; ?></td>
-                            <td><?php echo $item['fullname']; ?></td>
-                            <td><?php echo $item['email']; ?></td>
-                            <td><?php echo $item['phone']; ?></td>
-                            <td><?php echo $item['status'] == 1 ? '<button type="submit" class="btn btn-success btn-sm" style="padding-left:11px; padding-right:11px">Active</button>' : '<button type="submit" class="btn btn-warning btn-sm">Passive</button>'; ?></td>
-                            <td><a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a></td>
-                            <td><a href="#" onclick="return confirm('Are you sure?');" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></a></td>
-                        </tr>
-                    <?php endforeach; ?>
-                <?php else : ?>
-                    <tr>
-                        <td colspan="7">
-                            <div class="alert alert-danger text-center">Không có người dùng</div>
-                        </td>
-                    </tr>
-                <?php endif; ?>
-            </tbody>
-
-            </td>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th width="5%">Serial</th>
+                <th>Fullname</th>
+                <th>Email</th>
+                <th>Phone number</th>
+                <th>Status</th>
+                <th width="5%">Sửa</th>
+                <th width="5%">Xóa</th>
             </tr>
-
-            </tbody>
-        </table>
-        <hr />
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
+        </thead>
+        <tbody>
+            <?php if (!empty($listAllUser)) : ?>
                 <?php
-
-                if ($page > 1) {
-                    $prevPage = $page - 1;
-                    echo '<li class="page-item"><a class="page-link" href="' . _WEB_HOST_ROOT . '?module=users&action=list&page=' . $prevPage . '">Trước</a></li>';
-                }
-
-
+                $count = 0; // Khởi tạo biến đếm để hiển thị số thứ tự
+                foreach ($listAllUser as $item) :
+                    $count++;
                 ?>
+                    <tr>
+                        <td><?php echo $count; ?></td>
+                        <td><?php echo $item['fullname']; ?></td>
+                        <td><?php echo $item['email']; ?></td>
+                        <td><?php echo $item['phone']; ?></td>
+                        <td><?php echo $item['status'] == 1 ? '<button type="submit" class="btn btn-success btn-sm" style="padding-left:11px; padding-right:11px">Active</button>' : '<button type="submit" class="btn btn-warning btn-sm">Passive</button>'; ?></td>
+                        <td><a href=<?php echo _WEB_HOST_ROOT . '?module=users&action=edit&id=' . $item['id'] ?> class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a></td>
+                        <td><a href="#" onclick="return confirm('Are you sure?');" class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></a></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <tr>
+                    <td colspan="7">
+                        <div class="alert alert-danger text-center">Không có người dùng</div>
+                    </td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
 
-                <?php
-                $begin = $page - 2;
-                $end = $page + 2;
-                if ($begin < 1) {
-                    $begin = 1;
-                }
-                if ($end >= $maxPage) {
-                    $end = $maxPage;
-                }
-                for ($index = $begin; $index <= $end; $index++) : ?>
-                    <li class="page-item <?php echo ($index == $page) ? 'active' : ''; ?>">
-                        <a class="page-link" href="<?php echo _WEB_HOST_ROOT . '?module=users'.$queryString.'&page=' . $index; ?>">
-                            <?php echo $index; ?>
-                        </a>
-                    </li>
-                <?php endfor; ?>
+        </td>
+        </tr>
 
-                <?php
-                if ($page < $maxPage) {
-                    $nextPage = $page + 1;
-                    echo '<li class="page-item"><a class="page-link" href="' . _WEB_HOST_ROOT . '?module=users'.$queryString.'&page='. $nextPage . '">Sau</a></li>';
-                }
+        </tbody>
+    </table>
+    <hr />
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <?php
 
-                ?>
-            </ul>
-        </nav>
+            if ($page > 1) {
+                $prevPage = $page - 1;
+                echo '<li class="page-item"><a class="page-link" href="' . _WEB_HOST_ROOT . '?module=users&action=list&page=' . $prevPage . '">Trước</a></li>';
+            }
+
+
+            ?>
+
+            <?php
+            $begin = $page - 2;
+            $end = $page + 2;
+            if ($begin < 1) {
+                $begin = 1;
+            }
+            if ($end >= $maxPage) {
+                $end = $maxPage;
+            }
+            for ($index = $begin; $index <= $end; $index++) : ?>
+                <li class="page-item <?php echo ($index == $page) ? 'active' : ''; ?>">
+                    <a class="page-link" href="<?php echo _WEB_HOST_ROOT . '?module=users' . $queryString . '&page=' . $index; ?>">
+                        <?php echo $index; ?>
+                    </a>
+                </li>
+            <?php endfor; ?>
+
+            <?php
+            if ($page < $maxPage) {
+                $nextPage = $page + 1;
+                echo '<li class="page-item"><a class="page-link" href="' . _WEB_HOST_ROOT . '?module=users' . $queryString . '&page=' . $nextPage . '">Sau</a></li>';
+            }
+
+            ?>
+        </ul>
+    </nav>
 
     </div>
 
